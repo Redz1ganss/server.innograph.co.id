ebeea7bd6304323a6478bdc9c2ce65f261a70175
Redz1ganss <55497551+Redz1ganss@users.noreply.github.com>
2019-09-19 09:40:17 +0700

    Create contact.php

---
+++ contact.php
@@ -0,0 +1,42 @@
+<html>
+<head>
+<style type="text/css">
+body{
+    background-color:#000;
+}
+h1 {
+  font-size: 80px;
+  letter-spacing: 4px;
+  margin-top:45%;
+  font-family: Georgia, serif;
+  background: linear-gradient(90deg, #000, #fff, #000);
+  background-repeat: no-repeat;
+  background-size: 80%;
+  animation: animate 2.3s linear infinite;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: rgba(255, 255, 255, 0);
+}
+@keyframes animate {
+  0% {
+    background-position: -700%;
+  }
+  100% {
+    background-position: 700%;
+  }
+}
+@-webkit-keyframes animate {
+  0% {
+    background-position: -700%;
+  }
+  100% {
+    background-position: 700%;
+  }
+}
+</style>
+<title>Pawned By Red-z1</title>
+</head>
+<body>
+<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+<center><h1>Hbd Jiancox :v</h1></center>
+</body>
+</html>
